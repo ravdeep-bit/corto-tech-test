88dbf0c0dd7616c6078db877ff80b3e614fb5e12
Ravdeep Saini <ravdeepsaini@Ravdeeps-MacBook-Pro.local>
2026-05-08 09:17:29 +1000

    revert auth fail

--- api-automation/config/env.ts
+++ api-automation/config/env.ts
@@ -4,7 +4,7 @@
 import 'dotenv/config';
 
 const BASE_URL = process.env.BASE_URL || 'https://restful-booker.herokuapp.com';
-const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admintest';
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admin';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'password123';
 
 export { BASE_URL, ADMIN_USERNAME, ADMIN_PASSWORD };
